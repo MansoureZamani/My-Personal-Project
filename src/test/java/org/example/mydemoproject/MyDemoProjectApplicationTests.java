a4db2696f129ef2b43b426e55f04c74ab7d903dd
mansoureh <mansooreh.zamani@gmail.com>
2025-01-08 15:08:54 +0330

    Initialize project skeleton

---
+++ src/test/java/org/example/mydemoproject/MyDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mydemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
